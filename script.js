57c6878d9617abfca3d97901497c9bbc6782d77c
akwilliams <akwilliamsaudio@gmail.com>
2017-09-28 21:21:41 -0700

    170928
    Continuing build progress

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-var parameters = {};
-function init(hold){
-	console.log(hold);
+var parameters = {present_subdivisions:[],sum_beats:0,subdivision_density:[]};
+
+function calcPrimarySubdivision(hold){
+	for(i=0;i<parameters.present_subdivisions.length;i++){
+		for(j=0;j<(parameters.sum_beats/(parameters.present_subdivisions[i]/parameters.present_subdivisions[parameters.present_subdivisions.length-1]));j++){
+			var beat = checkForBeat(j*(parameters.present_subdivisions[i]/parameters.present_subdivisions[parameters.present_subdivisions.length-1]));
+			if(beat){
+				if(typeof parameters.subdivision_density[j]!== "number"){
+					parameters.subdivision_density[j]=0;
+				}
+				parameters.subdivision_density[j]++;
+			}
+
+		}
+	}
+}
+
+function checkForBeat(value){
+
+	//if()
 }
